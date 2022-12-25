5105ac6a91d5861afc91effb27c4dc74182b1e36
Andrii <sushkoandy1@gmail.com>
2022-12-25 23:48:21 +0200

    fix timing and library

--- src/js/uparrow.js
+++ src/js/uparrow.js
@@ -2,7 +2,7 @@ function backToTop() {
 	let button = $(".up__button");
 
 	$(window).on("scroll", () => {
-		if ($(this).scrollTop() >= 350) {
+		if ($(this).scrollTop() >= 1500) {
 			button.fadeIn();
 		} else {
 			button.fadeOut();
@@ -11,7 +11,7 @@ function backToTop() {
 
 	button.on("click", (e) => {
 		e.preventDefault();
-		$("html").animate({ scrollTop: 0 }, 1000);
+		$("html").animate({ scrollTop: 0 }, 250);
 	});
 }
 
