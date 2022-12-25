70a791b23416b50f71379287535c82212e6af556
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2022-12-25 10:17:12 +0200

    modal-product-card

--- src/js/body-no-scroll.js
+++ src/js/body-no-scroll.js
@@ -1,15 +1,41 @@
+// modal-product-card
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-ovellay]'),
+    openBtnModalProtuctCard: document.querySelector(
+      '[data-modal-open-product-card]'
+    ),
+    closeBtnModalProtuctCard: document.querySelector(
+      '[data-modal-close-product-card]'
+    ),
+    modalProtuctCard: document.querySelector('[data-modal-product-card]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openBtnModalProtuctCard.addEventListener('click', toggleModal);
+  refs.closeBtnModalProtuctCard.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modalProtuctCard.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
+
+// modal-read-more
+
+(() => {
+  const refs = {
+    openBtnModalReadMore: document.querySelector('[data-modal-open-read-more]'),
+    closeBtnModalReadMore: document.querySelector(
+      '[data-modal-close-read-more]'
+    ),
+    modalReadMore: document.querySelector('[data-modal-read-more]'),
+  };
+
+  refs.openBtnModalReadMore.addEventListener('click', toggleModal);
+  refs.closeBtnModalReadMore.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modalReadMore.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
 })();
