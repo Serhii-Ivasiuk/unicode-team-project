c67b0796b10b13b85754133305a37610efa3bf5d
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2022-12-25 10:51:20 +0200

    modal-read-more

--- src/js/body-no-scroll.js
+++ src/js/body-no-scroll.js
@@ -8,14 +8,12 @@
     closeBtnModalProtuctCard: document.querySelector(
       '[data-modal-close-product-card]'
     ),
-    modalProtuctCard: document.querySelector('[data-modal-product-card]'),
   };
 
   refs.openBtnModalProtuctCard.addEventListener('click', toggleModal);
   refs.closeBtnModalProtuctCard.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modalProtuctCard.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
 })();
@@ -28,14 +26,12 @@
     closeBtnModalReadMore: document.querySelector(
       '[data-modal-close-read-more]'
     ),
-    modalReadMore: document.querySelector('[data-modal-read-more]'),
   };
 
   refs.openBtnModalReadMore.addEventListener('click', toggleModal);
   refs.closeBtnModalReadMore.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modalReadMore.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
 })();
