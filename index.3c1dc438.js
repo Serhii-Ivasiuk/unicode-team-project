c816d42f5269bc401ac6a0c3d4c49331e14ae514
Serhii-Ivasiuk <111089930+Serhii-Ivasiuk@users.noreply.github.com>
2022-12-25 22:41:57 +0000

    Deploying to gh-pages from @ Serhii-Ivasiuk/unicode-team-project@07e4137bdd69f4bf6d8a48074c30f577acdd39c3 🚀

---
+++ index.3c1dc438.js
@@ -0,0 +1,2 @@
+function backToTop(){var o=this,n=$(".up__button");$(window).on("scroll",(function(){$(o).scrollTop()>=1500?n.fadeIn():n.fadeOut()})),n.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},250)}))}backToTop();
+//# sourceMappingURL=index.3c1dc438.js.map
