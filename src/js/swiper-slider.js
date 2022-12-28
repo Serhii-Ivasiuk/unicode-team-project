9f8b387692dd53ed382efc425ba144eff060c41c
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2022-12-28 15:38:40 +0200

    added autoplay and breakpoints

--- src/js/swiper-slider.js
+++ src/js/swiper-slider.js
@@ -3,11 +3,31 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  spaceBetween: 20,
+  // spaceBetween: 20,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+
+  autoplay: {
+    dalay: 9000,
+    stopOnSlide: false,
+    disableOnInteraction: false,
+  },
+
+  speed: 5500,
+
+  breakpoints: {
+    320: {
+      spaceBetween: 10,
+    },
+    480: {
+      spaceBetween: 15,
+    },
+    768: {
+      spaceBetween: 20,
+    },
   },
 });
