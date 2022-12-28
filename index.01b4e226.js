ba8225a1be3772f5dd591a7ce59c46f6b5273246
Serhii-Ivasiuk <111089930+Serhii-Ivasiuk@users.noreply.github.com>
2022-12-28 21:48:33 +0000

    Deploying to gh-pages from @ Serhii-Ivasiuk/unicode-team-project@04dca90fe4ffc7518890b3476a301774c4437534 🚀

---
+++ index.01b4e226.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:15e3,stopOnSlide:!1,disableOnInteraction:!1},speed:1e3,breakpoints:{320:{spaceBetween:10},480:{spaceBetween:10},768:{spaceBetween:15},1200:{spaceBetween:20}}});
+//# sourceMappingURL=index.01b4e226.js.map
--- index.b5667bee.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,spaceBetween:20,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.b5667bee.js.map
